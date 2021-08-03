826af2f10fdf581fe168341ea1bfbaafa622051c
Mariami Tetradze <mariamtetradze9@gmail.com>
2021-08-03 17:40:53 +0400

    add SQLJDBCUtil and test

---
+++ src/test/java/SQLDataTest.java
@@ -0,0 +1,40 @@
+import com.codeborne.selenide.SelenideElement;
+import org.testng.annotations.Test;
+
+import java.sql.*;
+
+import static com.codeborne.selenide.Configuration.startMaximized;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SQLDataTest {
+    public SQLDataTest() {
+        startMaximized=true;
+    }
+
+    @Test
+    public void getDBValues() throws SQLException {
+        open("https://demoqa.com/automation-practice-form");
+        SelenideElement firstNameInput = $("#firstName");
+        SelenideElement lastNameInput = $("#lastName");
+        SelenideElement numberInput = $("#userNumber");
+
+        Connection conn = SQLJDBCUtil.getConnection();
+        String query = "select *  from studentsInfo;";
+        Statement stmt = conn.createStatement();
+        ResultSet rs= stmt.executeQuery(query);
+
+       /* int rows = rs.getRow();
+        rs.next();
+        System.out.println(rs.getString(2));
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int cols = rsmd.getColumnCount();
+*/
+        while (rs.next()) {
+            firstNameInput.setValue(rs.getString(2));
+            lastNameInput.setValue(rs.getString(3));
+            numberInput.setValue(rs.getString(4));
+        }
+
+        }
+
+    }
